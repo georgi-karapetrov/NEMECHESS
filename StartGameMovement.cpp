815911b1e66191566599ec6b14ef848561b8bf1a
Georgi Karapetrov <georgi.karapetrov1994@gmail.com>
2014-10-14 15:06:11 +0300

    Added new StartGameMovement class. Model now uses the Undo/Redo stacks. Changed Undo/Redo. Fixed bug with isInCheck().

---
+++ StartGameMovement.cpp
@@ -0,0 +1,5 @@
+#include "StartGameMovement.h"
+
+StartGameMovement::StartGameMovement()
+{
+}
---
+++ StartGameMovement.h
@@ -0,0 +1,10 @@
+#ifndef STARTGAMEMOVEMENT_H
+#define STARTGAMEMOVEMENT_H
+
+class StartGameMovement : public Movement
+{
+public:
+    StartGameMovement();
+};
+
+#endif // STARTGAMEMOVEMENT_H
